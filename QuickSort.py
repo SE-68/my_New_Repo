a0885ea8b5586f71a1ddeeda6fa68808f836d0c7
SE-68 <dlcjdals68@naver.com>
2024-09-19 14:27:44 +0900

    Two files have been modified

--- QuickSort.py
+++ QuickSort.py
@@ -3,8 +3,8 @@ def qsort(a, low, high):
         pivot = partition(a, low, high)
         qsort(a, low, pivot-1)
         qsort(a, pivot+1, high)
-aaaaa
 
+#Partition algorithm for the quick sort
 def partition(a, pivot, high):
     i = pivot+1
     j = high
@@ -22,7 +22,7 @@ def partition(a, pivot, high):
     a[pivot], a[j] = a[j], a[pivot]
     return j
 
-
+# Test codes
 a = [54,88,77,26,93,17,49,10,17,77,11,31,22,44,17,20]
 print('정렬 전:\t', a)  
 qsort(a, 0, len(a)-1)
